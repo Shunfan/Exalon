263a1a1f2b7b0531742aba3a139e52e298a4cb24
Shunfan Du <du@shunfan.io>
2016-07-27 00:13:26 -0400

    Add "Categories" table view and "Add a Category" table view including their table view controllers

---
+++ Exalon/CategoryTableViewController.swift
@@ -0,0 +1,109 @@
+//
+//  CategoryTableViewController.swift
+//  Exalon
+//
+//  Created by Shunfan Du on 7/26/16.
+//  Copyright © 2016 Rose-Hulman. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewController: UITableViewController {
+    let categoryCellIdentifier = "CategoryCell"
+    
+    // Example data for demo use
+    let depositList = ["Paycheck", "Gift", "Cash", "Transfer"];
+    let withdrawList = ["Book", "Viedo Game", "Food", "Gas"];
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 4
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch (section) {
+        case 0:
+            return "Deposit"
+        case 1:
+            return "Withdraw"
+        default:
+            return "NA"
+        }
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(categoryCellIdentifier, forIndexPath: indexPath)
+
+        switch (indexPath.section) {
+        case 0:
+            cell.textLabel?.text = depositList[indexPath.row]
+        case 1:
+            cell.textLabel?.text = withdrawList[indexPath.row]
+        default:
+            cell.textLabel?.text = "Other"
+        }
+
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
